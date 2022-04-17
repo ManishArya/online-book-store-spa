42de4a883e214b3f1e227c21dba8e53f6961bc80
Manish <manisharya26@gmail.com>
2022-04-17 18:35:13 +0530

    first commit

--- src/app/shared/app-spinner/app-spinner.component.ts
+++ src/app/shared/app-spinner/app-spinner.component.ts
@@ -1,15 +1,26 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-spinner',
   templateUrl: './app-spinner.component.html',
   styleUrls: ['./app-spinner.component.scss']
 })
-export class AppSpinnerComponent implements OnInit {
+export class AppSpinnerComponent implements OnChanges, OnInit {
   @Input() public isWaiting: boolean;
   @Input() public isFullPage: boolean = false;
+  @Input() public nonBlocking: boolean = false;
 
-  constructor() {}
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
-  ngOnInit(): void {}
+  public ngOnChanges(): void {
+    if (!this.isFullPage && !this.nonBlocking) {
+      this.renderer.setStyle(this.el.nativeElement.parentElement, 'position', this.isWaiting ? 'relative' : '');
+    }
+  }
+
+  public ngOnInit() {
+    if (this.isFullPage || this.nonBlocking) {
+      document.body.append(this.el.nativeElement);
+    }
+  }
 }
