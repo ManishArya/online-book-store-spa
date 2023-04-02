192bff39da87fca252ec16b6daefe910f668b0ab
Manish <manish>
2023-04-02 17:17:25 +0530

    small changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  authApiEndPoint: 'http://localhost:3000/api',
-  bookApiEndPoint: 'https://localhost:5001/api',
+  authApiEndPoint: 'http://localhost:3000/api/v1',
+  bookApiEndPoint: 'https://localhost:5001/api/v1',
   cartApiEndPoint: 'https://localhost:7227/api/v1'
 };
