c5cb041df31c57073a8bacb40572f1cecbede59d
Manish <manish>
2023-05-01 19:30:17 +0530

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  authApiEndPoint: 'http://35.202.79.31/api/v1',
-  bookApiEndPoint: 'http://34.70.237.105/api/v1',
+  authApiEndPoint: '',
+  bookApiEndPoint: '',
   cartApiEndPoint: 'https://localhost:7227/api/v1'
 };
